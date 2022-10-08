0fb883af657ce988919a458f78588e66caca017d
Zoul Aimi <mdzoul@gmail.com>
2022-10-08 17:44:44 +0800

    Allows user to change pencolor

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from turtle import Turtle, Screen
 
-bob = Turtle()
-screen = Screen()
+
+def draw():
+	"""Allows user to choose a colour and then draw"""
+	colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "black"]
+	user_color = screen.textinput(title="Color", prompt="""Choose a color:
+(red, orange, yellow, green, blue, purple, pink, black)""")
+	if user_color in colors:
+		bob.color(user_color)
+	keystrokes()
 
 
 def forward():
 	bob.fd(10)
-	
+
 
 def backward():
 	bob.back(10)
@@ -18,19 +25,30 @@ def right():
 
 def left():
 	bob.left(10)
-	
-	
+
+
 def reset():
 	bob.reset()
 
 
-screen.listen()
+def keystrokes():
+	"""Keystrokes available, including reset"""
+	screen.listen()
+	
+	screen.onkey(fun=draw, key="c")
 
-screen.onkeypress(fun=forward, key="w")
-screen.onkeypress(fun=backward, key="s")
-screen.onkeypress(fun=right, key="d")
-screen.onkeypress(fun=left, key="a")
+	screen.onkeypress(fun=forward, key="w")
+	screen.onkeypress(fun=backward, key="s")
+	screen.onkeypress(fun=right, key="d")
+	screen.onkeypress(fun=left, key="a")
+	
+	screen.onkey(fun=reset, key="r")
 
-screen.onkey(fun=reset, key="c")
+
+bob = Turtle()
+screen = Screen()
+screen.setup(width=700, height=500)
+
+draw()
 
 screen.exitonclick()
